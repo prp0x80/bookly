24fc000b36df6e328b6a6cdb6bd5f61b6295d013
prpox80 <prashant.patel@gmx.com>
2025-01-05 23:03:21 +0000

    Moved fastapi entry to main

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+
+from src.books.routes import book_router
+from src.auth.routes import auth_router
+from src.db.main import init_db
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Application started")
+    await init_db()
+    yield
+    print("Application stopped")
+
+
+version = "v1"
+
+app = FastAPI(title="Bookly",
+              description="A REST API for a book review web service", version=version, lifespan=lifespan)
+
+app.include_router(book_router, prefix=f"/api/{version}/books", tags=["books"])
+app.include_router(auth_router, prefix=f"/api/{version}/auth", tags=["auth"])
