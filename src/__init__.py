efcd13d6e37b4c915cb67c61bddfe1be96931bc5
prpox80 <prashant.patel@gmx.com>
2025-01-05 22:59:33 +0000

    Added auth router

--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 
 from src.books.routes import book_router
+from src.auth.routes import auth_router
 from src.db.main import init_db
 
 
@@ -19,3 +20,4 @@ app = FastAPI(title="Bookly",
               description="A REST API for a book review web service", version=version, lifespan=lifespan)
 
 app.include_router(book_router, prefix=f"/api/{version}/books", tags=["books"])
+app.include_router(auth_router, prefix=f"/api/{version}/auth", tags=["auth"])
